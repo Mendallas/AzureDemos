e54b31155db9b839c7f59f4aefc5640ca91a9798
Charles Boudry <chboudry@microsoft.com>
2018-09-27 16:43:08 +0200

    improving

--- DailyAutomationSolution/Demo_Daily_Solution.ps1
+++ DailyAutomationSolution/Demo_Daily_Solution.ps1
@@ -1,29 +1,57 @@
+#requires -RunAsAdministrator
 param(
-    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
     [string]$ResourceGroupName = 'ADMIN_DAILY',
 
-    [Parameter(Mandatory=$true)]
-    [string]$Location ='WestEurope',
+    [ValidateNotNullOrEmpty()]
+    [string]$Location = 'WestEurope',
     
-    [Parameter(Mandatory=$true)]
-    [string]$Filepath,
+    [ValidateNotNullOrEmpty()]
+    [string]$RunbookURL = 'https://raw.githubusercontent.com/Mendallas/AzureDemos/master/DailyAutomationSolution/Sources/runbook_ResourceGroupDailyRefresh.ps1',
 
-    [string]$AutomationAccountName = 'SubscriptionAutomation'
+    [ValidateNotNullOrEmpty()]
+    [string]$AutomationAccountName = 'SubscriptionAutomation',
+
+    [ValidateNotNullOrEmpty()]
+    [string]$AutomationConnectionName = "test",
+
+    [ValidateNotNullOrEmpty()]
+    [string]$KeyVaultName = "kv89232390"
 )
 
-
-## if the resource group already exist, we will delete it first
-$AzureResourceGroup = Get-AzureRmResourceGroup -name $ResourceGroupName -ErrorAction 'SilentlyContinue'
-if($AzureResourceGroup)
-{
-    Remove-AzureRmResourceGroup -name $ResourceGroupName -Force
-    $AzureResourceGroup = Get-AzureRmResourceGroup -name $ResourceGroupName -ErrorAction 'SilentlyContinue'
-    if($AzureResourceGroup)
-    {
-        write-error "Not able to delete the Group"
+function Check-AzurePrerequisites{
+    $AzureContext = Get-AzureRmContext
+    if(-not $AzureContext.Environment) {
+        Connect-AzureRmAccount
+        Get-AzureRmSubscription
+        $SubName = Read-Host "Subscription Name"
+        Select-AzureRmSubscription $SubName
     }
 }
 
+function CustomImport-AzureRmAutomationRunbook{
+}
+
+# If not already in Azure Environment we connect to it (graphical interface is then needed)
+# Please note that if you are using Visual Studio Code the Authent windows will be on the foreground https://github.com/Microsoft/vscode/issues/42356
+Check-AzurePrerequisites
+
+# New Resource Group
 New-AzureRmResourceGroup -name $ResourceGroupName -Location $location
+
+# New Azure Automation Account
 New-AzureRmAutomationAccount -ResourceGroupName $ResourceGroupName -Name $AutomationAccountName -Location $Location
-Import-AzureRmAutomationRunbook -Path $filepath -ResourceGroup $ResourceGroupName -AutomationAccountName $AutomationAccountName -Type PowerShell
+
+# new Azure Automation Run as Account 
+./New-RunAsAccount.ps1 -ResourceGroup $ResourceGroupName -AutomationAccountName $AutomationAccountName -SubscriptionId (Get-AzureRmContext).Subscription.Id -ApplicationDisplayName "fjsdjflsdfjlksdf" -SelfSignedCertPlainPassword "msjfklsdjflksjdfl2030123¨!!?" -CreateClassicRunAsAccount $false
+
+# $SubscriptionID = (Get-AzureRmContext).Subscription.Id
+# $FieldValues = @{"AutomationCertificateName"="ContosoCertificate";"SubscriptionID"=$SubscriptionID}
+# New-AzureRmAutomationConnection -Name $AutomationConnectionName -ConnectionTypeName Azure -ConnectionFieldValues $FieldValues -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName
+
+# I won't be using Import-AzureRmAutomationRunbook from the AzureRM module because it is not yet able to import from url, only drive path https://github.com/Azure/azure-powershell/issues/1640 
+# Import-AzureRmAutomationRunbook -Path $filepath -ResourceGroup $ResourceGroupName -AutomationAccountName $AutomationAccountName -Type PowerShell
+# However thanks to the Azure CmdLet being open source, we can easily recreate our own import function
+# https://github.com/Azure/azure-powershell/blob/preview/src/ResourceManager/Automation/Commands.Automation/Cmdlet/ImportAzureAutomationRunbook.cs
+# https://github.com/Azure/azure-powershell/blob/preview/src/ResourceManager/Automation/Commands.Automation/Common/AutomationClient.cs 
+CustomImport-AzureRmAutomationRunbook -Url $RunbookURL -ResourceGroup $ResourceGroupName -AutomationAccountName $AutomationAccountName -Type PowerShell
